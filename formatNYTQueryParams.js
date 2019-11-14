c6137935f06f84fe22787d108fe48ff9f8514ab8
Alyssa Bonesteel <anbonesteel@gmail.com>
2019-11-13 21:04:48 -0500

    remove format file

--- formatNYTQueryParams.js
+++
@@ -1,5 +0,0 @@
-// function formatNYTQueryParams(params) {
-//     const NYTqueryItems = Object.keys(params)
-//         .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
-//     return NYTqueryItems.join('&');
-// }
